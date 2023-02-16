f782ab0524757fea3746ae292b3db7289f215805
garry <garry4978@gmail.com>
2023-02-16 04:38:19 +0300

    Основы алгоритмов часть 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Arrays;
+
 public class Main {
+
+//    задача:
+//    написать свою реализацию ArrayList. В данной задаче он должен работать только со строками.
+//    Напишите реализацию этого интерфейса, выполнив все его методы. В качестве хранилища используйте массив.
+//    В конструкторе должен задаваться размер массива внутри.
+//    Список не должен добавлять или хранить в себе null. Т. е. в случае удаления элемента нужно смещать все
+//    элементы на ячейку влево, а при добавлении в середину или начало — на ячейку вправо.
+//    По желанию можно реализовать расширение массива.
+//    Рекомендуется написать свои исключения и выбрасывать их в тех ситуациях, которые описаны в интерфейсе.
+//    Если в какой-то из методов в качестве параметра приходит null, выбросить исключение.
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyArrayList myList = new MyArrayList();
+        MyArrayList myOtherList = new MyArrayList(2);
+
+        System.out.println(myList.size);
+
+        myList.add("тест1"); myList.add("тест2"); myList.add("тест3"); myList.add("тест4");
+        myOtherList.add("тест1"); myOtherList.add("тест2"); myOtherList.add("тест3"); myOtherList.add("тест4");
+
+
+        System.out.println(myList);
+        System.out.println(myOtherList);
+
+        System.out.println("myList.remove(\"test4\") = " + myList.remove("тест4"));
+        System.out.println("myList.remove(1) = " + myList.remove(1));
+        System.out.println("myList.add(1, \"тест2\") = " + myList.add(1, "тест2"));
+        System.out.println(myList);
+        System.out.println("myList.contains(\"тест\") = " + myList.contains("тест"));
+        System.out.println("myList.contains(\"тест3\") = " + myList.contains("тест3"));
+        System.out.println("myList.indexOf(\"тест3\") = " + myList.indexOf("тест3"));
+        System.out.println("myList.lastIndexOf(\"тест3\") = " + myList.lastIndexOf("тест3"));
+        System.out.println("myList.equals(myOtherList) = " + myList.equals(myOtherList));
+        System.out.println("myOtherList.toArray() = " + Arrays.toString(myOtherList.toArray()));
+        myOtherList.clear();
+        System.out.println("myOtherList.isEmpty() = " + myOtherList.isEmpty());
+
+
     }
 }
---
+++ src/MyArrayList.java
@@ -0,0 +1,140 @@
+import java.util.Arrays;
+
+
+public class MyArrayList implements StringList {
+    final int INCREMENT = 0x3;   // для тестов = 3
+    public int size ;
+    private String[] list ;
+
+    public MyArrayList() { clear(); }
+
+    public MyArrayList(int size) {
+        list = new String[size];
+    }
+
+    @Override
+    public String add(String item) {
+        if( list[list.length-1] != null){
+        var newList = new String[list.length + INCREMENT];
+        System.arraycopy(list,0, newList,0, list.length);
+        list = newList;
+        }
+        return list[size++]=item;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        checkBounds(index);
+        var inc = INCREMENT;
+        if (list.length != size) inc = 0;
+
+        String[] buffer = new String[list.length + inc];
+        System.arraycopy(list, 0, buffer, 0, index);
+        System.arraycopy(list, index, buffer, index + 1, size - index);
+        buffer[index] = item;
+
+        list = buffer;
+        size++;
+        return item;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        checkBounds(index);
+        return list[index]=item;
+    }
+
+    @Override
+    public String remove(String item) {
+
+        if (!contains(item)) throw new IllegalArgumentException(" элемент отсутствует в списке ");
+        this.remove(indexOf(item));
+        return item;
+    }
+
+    @Override
+    public String remove(int index) {
+
+        checkBounds(index);
+        if (list[index] == null) throw new IllegalArgumentException(" элемент отсутствует в списке ");
+        size--;
+        var result = list[index];
+        System.arraycopy(list, index + 1, list, index, size-index );
+        list[size]=null;
+        return result;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        for ( String s: list )
+            if ( s != null && s.equals(item))
+                return true;
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        for (int i = 0; i < size; i++)
+            if ( list[i].equals(item))
+                return i;
+
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        for (int i = size-1 ; i >= 0; i--)
+            if (list[i].equals(item))
+                return i;
+
+        return -1;
+    }
+
+    @Override
+    public String get(int index) {
+        checkBounds(index);
+        return list[index];
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+
+        if( size != otherList.size()) return false;
+        for (int i = 0; i < size; i++)
+            if (!list[i].equals(otherList.get(i)))
+                return false;
+
+        return true;
+    }
+
+
+    @Override
+    public int size() { return size; }
+
+    @Override
+    public boolean isEmpty() { return size == 0; }
+
+    @Override
+    public void clear() {
+        size = 0;
+        list = new String[INCREMENT];
+        }
+
+    @Override
+    public String[] toArray() { return Arrays.copyOf(list,size); }
+
+    public void checkBounds(int index) {
+        if ( index < 0 || index > list.length )
+            throw new ArrayIndexOutOfBoundsException ( " выход индекса за границы массива " );
+    }
+
+    @Override
+    public String toString() {
+        return "MyArrayList{" +
+                "INC=" + INCREMENT +
+                ", size=" + size +
+                ", list=" + Arrays.toString(list) +
+                '}';
+    }
+
+}
